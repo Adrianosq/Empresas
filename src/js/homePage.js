7b5965c5d6c82a9b88b24c27c49071050b538596
Adriano Souza <adrianos924@gmail.com>
2022-11-06 19:58:52 -0400

    fix: filter na homePage

--- src/js/homePage.js
+++ src/js/homePage.js
@@ -1,7 +1,12 @@
 import { menuResponsivo } from "./menuResponsivo.js";
 import { cadastrarPage, loginPage } from "./redirecionar.js";
+import { homePageAllEmpresas, homePageFilter } from "./render.js";
+import { getAllEmpresas } from "./request.js";
 
 
 menuResponsivo()
 loginPage()
 cadastrarPage()
+const data = await getAllEmpresas()
+homePageAllEmpresas(data)
+homePageFilter()
